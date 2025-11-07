928f13afc0e3786679722e29072f164150899061
Julia Mequilini <jujulrocha@gmail.com>
2025-11-07 14:33:12 -0300

    +vue-chartjs

---
+++ src/plugins/chart.ts
@@ -0,0 +1,18 @@
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+} from 'chart.js'
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale
+)
