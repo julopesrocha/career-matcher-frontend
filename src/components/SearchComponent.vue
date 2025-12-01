e5f73edaeaca18e1c2f2fe2a11615fa14e38ae91
Julia Mequilini <jujulrocha@gmail.com>
2025-12-01 12:35:20 -0300

    + filtering items

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,52 +1,44 @@
 <template>
-  <v-row dense justify="center">
-    <v-col cols="12">
-      <v-select
-        chips
-        ref="searchField"
-        label="Busca"
-        :items="items"
-        multiple
-        clearable
-        class="search-box"
-        hide-details
-        rounded="pill"
-        bg-color="white"
-        variant="solo"
-        v-model="selected"
-      >
-        <template #selection="{ item, index }">
-          <!-- Mostra só os 2 primeiros chips -->
-          <v-chip v-if="index < 2" class="mr-1" size="small">
-            {{ item.title }}
-          </v-chip>
+  <v-select
+    v-model="value"
+    :items="items"
+    label="Selecione"
+    multiple
+    clearable
+    rounded="pill"
+    hide-details
+    variant="solo"
+  >
+    <template v-slot:selection="{ item, index }">
+      <v-chip v-if="index < 3" :text="item.title"></v-chip>
 
-          <!-- Mostra o +N ONLY quando index == 2 -->
-          <span v-else-if="index === 2" class="text-grey text-caption align-self-center">
-            (+{{ selected.length - 2 }} outros)
-          </span>
-        </template>
-      </v-select>
-    </v-col>
-  </v-row>
+      <span v-if="index === 3" class="text-grey text-caption align-self-center">
+        (+{{ value.length - 3 }} others)
+      </span>
+    </template>
+  </v-select>
 </template>
-
 <script setup lang="ts">
-import { ref } from 'vue'
+import { shallowRef } from 'vue'
 
-const selected = ref<string[]>([])
-
+const value = shallowRef([
+  'Frontend',
+  'Backend',
+  'Senior',
+  'Pleno',
+  'Junior',
+  'Remoto',
+  'Presencial',
+  'Híbrido',
+])
 const items = [
-  'California',
-  'Colorado',
-  'Florida',
-  'Georgia',
-  'Texas',
-  'Wyoming',
-  'Rio de Janeiro',
-  'Londres',
-  'Geodgia',
-  'Texads',
-  'Wyomsing',
+  'Frontend',
+  'Backend',
+  'Senior',
+  'Pleno',
+  'Junior',
+  'Remoto',
+  'Presencial',
+  'Híbrido',
 ]
 </script>
